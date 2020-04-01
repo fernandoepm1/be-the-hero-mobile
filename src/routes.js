8e6570f128bc2a59a16ccd6d82542c540d0322af
Fernando Eufrásio <fernando.epm1@gmail.com>
2020-04-01 19:06:10 -0300

    Create routes and app stack navigation

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Incidents from './pages/Incidents';
+import IncidentDetails from './pages/IncidentDetails';
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen name="Details" component={IncidentDetails} />
+      </AppStack.Navigator>
+
+    </NavigationContainer>
+  );
+}
